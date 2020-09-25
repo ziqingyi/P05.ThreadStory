5d99f2303cfefae05e3f2ae7e9d9b9d2cc557426
adrian_sun <ziqingyix@gmail.com>
2020-09-25 15:23:37 +1000

    Add project files.

---
+++ P05.ThreadModel/StoryCharacter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P05.ThreadModel
+{
+    public class StoryCharacter
+    {
+        public string Name { get; set; }
+
+        public ConsoleColor color { get; set; }
+
+        public List<string> Experience { get; set; }
+    }
+}
