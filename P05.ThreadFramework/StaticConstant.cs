9844897f1f2550f1a7c286b26c4c557f6daec318
adrian_sun <ziqingyi@qq.com>
2020-09-26 17:48:15 +1000

    xml helper

--- P05.ThreadFramework/StaticConstant.cs
+++ P05.ThreadFramework/StaticConstant.cs
@@ -10,6 +10,8 @@ namespace P05.ThreadFramework
     public class StaticConstant
     {
 
+        //get serialized data path
+        public static string SerializeDataPath = ConfigurationManager.AppSettings["SerializeDataPath"];
 
 
         #region file location
